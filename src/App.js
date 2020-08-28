a0a6ea9328c729de78604d8605571c010dee2ec5
NahidKhan1 <arnahidkhanbpi@gmail.com>
2020-08-28 23:54:49 +0600

    finally upload

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import Course from './components/course/Course';
+import Shop from './components/shop/Shop';
+
+
+
+
+
+function App() {
+
+  return (
+    <div>
+<Course></Course>
+<Shop></Shop>
+
+
+
+     </div>
+  );
+}
+
+export default App;
