fd5c8ca01fbeae04df247f88457eab82bc3f33ab
NahidKhan1 <arnahidkhanbpi@gmail.com>
2020-08-28 23:58:04 +0600

    finally upload

--- src/FakeData/Data.js
+++ src/FakeData/Data.js
@@ -7,17 +7,16 @@ let Data = [
     },
     {
         id: 2,
-        name: 'BootStrap-Framwork',
-        price: 89.36,
-        img: 'https://cdn.auth0.com/blog/new-bootstrap4/logo.png'
-    },
-    {
-        id: 3,
         name: 'CSS-CSS3',
         price: 50.99,
         img: 'https://sabe.io/classes/css/hero.png'
     },
-   
+    {
+        id: 3,
+        name: 'BootStrap-Framwork',
+        price: 89.36,
+        img: 'https://cdn.auth0.com/blog/new-bootstrap4/logo.png'
+    },
     {
         id: 4,
         name: 'Modern-React JS',
@@ -25,56 +24,49 @@ let Data = [
         img: 'https://www.freecodecamp.org/news/content/images/2020/02/Ekran-Resmi-2019-11-18-18.08.13.png'
 
     },
-
     {
         id: 5,
-        name: 'C# - Basice to Advance and core part',
-        price:199.99,
-        img: 'https://pluralsight.imgix.net/paths/path-icons/csharp-e7b8fcd4ce.png'
-    }
-    {
-        id: 6,
         name: 'Modern-Angular JS',
         price: 99.99,
         img: 'https://peerbits-wpengine.netdna-ssl.com/wp-content/uploads/2018/10/angular-virtual-scroll-drag-drop-main.jpg'
     },
     {
-        id: 7,
+        id: 6,
         name: 'Moder-Vue JS',
         price: 90.50,
         img: 'https://miro.medium.com/max/3920/1*oZqGznbYXJfBlvGp5gQlYQ.jpeg'
     },
     {
-        id: 8,
+        id: 7,
         name: 'Javascript-ES6',
         price: 69.25,
         img: 'https://hackernoon.com/hn-images/1*6-bW-_sbW9nhe7Wa8uFPKQ.png'
     },
     {
-        id: 9,
+        id: 8,
         name: 'Python',
         price: 189.87,
         img: 'https://www.filepicker.io/api/file/BFMMlbcQvml9HSqXcvNp'
     },
     {
-        id: 10,
+        id: 9,
         name: 'Django-FrameWork',
         price: 45.99,
         img: 'https://themeforest.img.customer.envatousercontent.com/files/180904265/djangopre.jpg?auto=compress%2Cformat&q=80&fit=crop&crop=top&max-h=8000&max-w=590&s=9625d406063207f9205a65eb7d1c6ff0'
     },
+
     {
         id: 11,
-        name: 'Wordpress-CMS',
-        price: 230.25,
-        img: 'https://softwarehamilton.com/wp-content/uploads/2015/05/wordpress-bg-medblue.png'
-    },
-    {
-        id: 12,
         name: 'Laravel-Framwork',
         price: 125.99,
         img: 'https://www.cloudways.com/blog/wp-content/uploads/install-laravel-3.jpg'
     },
-    
+    {
+        id: 12,
+        name: 'Wordpress-CMS',
+        price: 230.25,
+        img: 'https://softwarehamilton.com/wp-content/uploads/2015/05/wordpress-bg-medblue.png'
+    },
     {
         id: 13,
         name: 'C++ - core part',
@@ -87,6 +79,11 @@ let Data = [
         price: 299.99,
         img: 'https://www.oracle.com/a/ocom/img/hp11-intl-java-logo.jpg'
     },
-    
+    {
+        id: 15,
+        name: 'C# - Basice to Advance and core part',
+        price:199.99,
+        img: 'https://pluralsight.imgix.net/paths/path-icons/csharp-e7b8fcd4ce.png'
+    }
 ]
 export default Data;
